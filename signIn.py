3207157ffadd70d98802cb00259536313fb525ec
quyle9 <qle@waverleysoftware.com>
2017-12-14 00:30:26 -0800

    a

--- signIn.py
+++ signIn.py
@@ -40,9 +40,3 @@ if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
     unittest.TextTestRunner(verbosity=2).run(suite)
 
-
-
-
-
-"test to upload the file" \
-"afafasdfa"
