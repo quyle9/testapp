2baec8f1e007732350600721b4d9cb20f44e4953
qtranwaverley <qtran@waverleysoftware.com>
2017-12-14 11:46:55 +0700

    test

--- signIn.py
+++ signIn.py
@@ -35,6 +35,7 @@ class MyTestCase(unittest.TestCase):
         error = self.driver.find_element_by_accessibility_id('Username or password invalid')
         self.assertTrue(error.is_displayed())
 
+unittest
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(MyTestCase)
