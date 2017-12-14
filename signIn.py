ea0c44364b0e866efd952bb25f6722c6722faa9b
quyle9 <qle@waverleysoftware.com>
2017-12-13 23:04:51 -0800

    adfaxxcvxvx

--- signIn.py
+++ signIn.py
@@ -41,7 +41,9 @@ if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite)
 
 
-"ASFAFA" \
-"ADSFASFAS" \
-"ASDFASFASDF" \
-"ASFASFA"
+""//test to push code to QQtestBranch""
+"ok done asdfa""
+"asfasf" \
+"asfadsf" \
+"asdfadsfasfasfa" \
+"aadfadsfa"
